8bbdfcccde25b1058c980a55c6a02bef3b3a2d51
Saheb Irani <sinjul.msbh@yahoo.com>
2019-02-28 12:26:36 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Data.DataView;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+using System;
+
+// CS0649 compiler warning is disabled because some fields are only 
+// assigned to dynamically by ML.NET at runtime
+#pragma warning disable CS0649
+
+namespace myApp
+{
+    class Program
+    {
+        // STEP 1: Define your data structures
+        // SampleData is used to provide training data, and as
+        // input for prediction operations
+        // - First 4 properties are inputs/features used to predict the label
+        // - Label is what you are predicting, and is only set when training
+        public class SampleData
+        {
+            [LoadColumn(0)]
+            public float SepalLength;
+
+            [LoadColumn(1)]
+            public float SepalWidth;
+
+            [LoadColumn(2)]
+            public float PetalLength;
+
+            [LoadColumn(3)]
+            public float PetalWidth;
+
+            [LoadColumn(4)]
+            public string Label;
+        }
+
+        // SamplePrediction is the result returned from prediction operations
+        public class SamplePrediction
+        {
+            [ColumnName("PredictedLabel")]
+            public string PredictedLabels;
+        }
+
+        static void Main(string[] args)
+        {
+            // STEP 2: Create a ML.NET environment  
+            MLContext mlContext = new MLContext();
+
+            // If working in Visual Studio, make sure the 'Copy to Output Directory'
+            // property of Sample-data.txt is set to 'Copy always'
+            IDataView trainingDataView = mlContext.Data.ReadFromTextFile<SampleData>(path: "sample-data.txt", hasHeader: false, separatorChar: ',');
+
+            // STEP 3: Transform your data and add a learner
+            // Assign numeric values to text in the "Label" column, because only
+            // numbers can be processed during model training.
+            // Add a learning algorithm to the pipeline. e.g.(What type of Sample is this?)
+            // Convert the Label back into original text (after converting to number in step 3)
+            var pipeline = mlContext.Transforms.Conversion.MapValueToKey("Label")
+                .Append(mlContext.Transforms.Concatenate("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"))
+                .AppendCacheCheckpoint(mlContext)
+                .Append(mlContext.MulticlassClassification.Trainers.StochasticDualCoordinateAscent(labelColumn: "Label", featureColumn: "Features"))
+                .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+            // STEP 4: Train your model based on the data set  
+            var model = pipeline.Fit(trainingDataView);
+
+            // STEP 5: Use your model to make a prediction
+            // You can change these numbers to test different predictions
+            var prediction = model.CreatePredictionEngine<SampleData, SamplePrediction>(mlContext).Predict(
+                new SampleData()
+                {
+                    SepalLength = 3.3f,
+                    SepalWidth = 1.6f,
+                    PetalLength = 0.2f,
+                    PetalWidth = 5.1f,
+                });
+
+            Console.WriteLine($"Predicted flower type is: {prediction.PredictedLabels}");
+
+            Console.WriteLine("Press any key to exit....");
+            Console.ReadLine();
+        }
+    }
+}
